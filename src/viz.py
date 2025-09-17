e74104c3f031807fb7c29600b9ed2a5d6789aaee
Jack Lecomte <jack.lecomte@mediatransports.com>
2025-09-17 17:04:39 +0200

    Added feature weight and confusion matrix. Started ROC curves

--- src/viz.py
+++ src/viz.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.metrics import confusion_matrix, precision_score, f1_score, \
-    accuracy_score
+    accuracy_score, roc_curve, auc
 
 
 class Viz:
@@ -230,3 +230,28 @@ class Viz:
         print(f'Precision: {precision:.3f}')
         print(f'Specificity: {specificity:.3f}')
         print(f'F1 Score: {f1:.3f}')
+
+
+    def roc_curve(self, y_pred_probas=None, y_test=None, labels=None) -> None:
+        # If single curve
+        if not isinstance(y_pred_probas, list):
+            y_pred_probas = [y_pred_probas]
+            y_test = [y_test] 
+            labels = ['ROC curve']
+
+        # Plot ROC curve
+        plt.figure(figsize=(8, 6))
+        
+        for y_pred_proba, y_true, label in zip(y_pred_probas, y_test, labels):
+            fpr, tpr, _ = roc_curve(y_true, y_pred_proba)
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, lw=2, label=f'{label} (AUC = {roc_auc:.3f})')
+
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate') 
+        plt.title('Receiver Operating Characteristic (ROC) Curve')
+        plt.legend(loc="lower right")
+        plt.show()
